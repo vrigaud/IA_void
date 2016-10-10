0a980e5f2bbb670c367bbb6eb47a700ba30e0755
Vincent Rigaud <vincent.rigaud@insa-lyon.fr>
2016-10-09 22:45:45 -0400

    [MODIF] Mise au point du squelette de la class Npc avant le merge avec le singleton map

--- MyBotLogic/Npc.cpp
+++ MyBotLogic/Npc.cpp
@@ -1,8 +1,11 @@
 #include "Npc.h"
-#include <windows.h>
+#include "Globals.h"
+
+#include <algorithm>
+
 
 Npc::Npc(unsigned int a_id, std::string a_path)
-    : m_currentState{ NONE }, m_nextState{ NONE }, m_id{ a_id }
+    : m_currentState{ NONE }, m_nextState{ EXPLORING }, m_id{ a_id }, m_goal{}, m_hasGoal{ false }, m_path{}, m_nextActions{}
 {
 #ifdef BOT_LOGIC_DEBUG_NPC
     m_logger.Init(a_path, "Npc_" + std::to_string(m_id) + ".log");
@@ -16,24 +19,121 @@ void Npc::update()
 {
     while (m_currentState != m_nextState)
     {
+        m_currentState = m_nextState;
         // TODO - Change State to another
         switch (m_currentState)
         {
         case(MOVING):
-
+            followPath();
             break;
         case(WAITING):
-
+            wait();
             break;
         case(EXPLORING):
-
+            explore();
             break;
         case(INTERACTING):
-
+            interact();
             break;
         case(ARRIVED):
-
+            m_nextState = ARRIVED; // May be useless atm
+            m_currentState = ARRIVED;
             break;
         }
     }
+}
+
+bool Npc::stopEverything()
+{
+    m_nextActions.clear();
+}
+
+void Npc::stopMoving()
+{
+    auto it = std::partition(std::begin(m_nextActions),
+        std::end(m_nextActions),
+        [](const Action& curAction) { return curAction.actionType == Action_Move;});
+
+    m_nextActions.erase(it, std::end(m_nextActions));
+}
+
+void Npc::stopInteract()
+{
+    auto it = std::partition(std::begin(m_nextActions),
+        std::end(m_nextActions),
+        [](const Action& curAction) { return curAction.actionType == Action_Interact;});
+
+    m_nextActions.erase(it, std::end(m_nextActions));
+}
+
+void Npc::unstackActions()
+{
+    while (m_nextActions.size()) 
+    {
+        Action& curAction{ m_nextActions.back() };
+        switch (curAction.actionType)
+        {
+        case Action_None:
+            // Do nothing
+            break;
+        case Action_Move:
+            moveForwardOnPath();
+            break;
+        case Action_Interact:
+            // TODO - do nothing atm
+            break;
+        }
+        m_nextActions.pop_back();
+    }
+}
+
+bool Npc::updatePath()
+{
+    // TODO - check path integrity and update if needed.
+
+    return true;
+}
+
+int Npc::getNextPathTile() const
+{
+    if (m_path.size() == 1)
+    {
+        return -1;
+    }
+    unsigned int index = m_path[m_path.size() - 2];
+    return index;
+}
+
+void Npc::explore()
+{
+    // TODO - explore the map with a lot of efficiency
+    if (m_hasGoal)
+    {
+        m_nextState = MOVING;
+        return;
+    }
+
+    // Ask map for potential direction
+
+    // Choose a direction
+
+    // Update next action in the vector
+}
+
+void Npc::followPath()
+{
+    // TODO - follow the path
+    // Get the direction between the two last nodes of m_path
+
+    // Update the action vector and push a move action
+}
+
+void Npc::wait()
+{
+    // TODO - Test why we are blocked ?
+}
+
+void Npc::interact()
+{
+    // TODO - interact with some fancy stuff
 }
--- MyBotLogic/Npc.h
+++ MyBotLogic/Npc.h
@@ -4,6 +4,7 @@
 #include "Logger.h"
 
 #include <string>
+#include <vector>
 
 #ifdef _DEBUG
 #define BOT_LOGIC_DEBUG_NPC
@@ -15,6 +16,7 @@
 #define BOT_LOGIC_NPC_LOG(logger, text, autoEndLine) 0
 #endif
 
+struct Action;
 
 class Npc 
 {
@@ -30,12 +32,69 @@ private:
 
     npcState m_currentState, m_nextState;
     unsigned int m_id;
+    unsigned int m_goal;
+    bool m_hasGoal;
+    std::vector<unsigned int> m_path;
+    std::vector<Action> m_nextActions;
     Logger m_logger;
 
 public:
     Npc(unsigned int a_id, std::string a_path);
 
+    // Main fonction for our Npc's FSM 
     void update();
+
+    // Stop everything the npc is doing
+    bool stopEverything();
+
+    // Remove all move action from next_action List
+    void stopMoving();
+
+    // Remove all interact action from next_action List
+    void stopInteract();
+
+    // Unstack m_nextAction and act according to action_type
+    void unstackActions();
+
+    // check path integrity, if pass is corrupted, try to find a new path return true if found
+    bool updatePath();
+    unsigned int getCurrentTileId()
+    {
+        return m_path.front();
+    }
+    int getNextPathTile()const;
+    unsigned int getPathSize() const
+    {
+        return m_path.size();
+    }
+    std::vector<unsigned int> getWholePath() const
+    {
+        return m_path;
+    }
+    
+    void setGoal(unsigned int a_id)
+    {
+        m_goal = a_id;
+    }
+    bool isOnGoalTile() const
+    {
+        return m_currentState == ARRIVED;
+    }
+    void moveForwardOnPath() // remove last tileId from m_path
+    {
+        m_path.pop_back();
+    }
+
+    std::vector<Action>& getActions()
+    {
+        return m_nextActions;
+    }
+
+private:
+    void explore();
+    void followPath();
+    void wait();
+    void interact();
 };
 
 
